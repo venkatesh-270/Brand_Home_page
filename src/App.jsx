ad509d2ed6dfb46e77d5fdffa5ad89e1ecd5005c
Venkatesh Yadav <venkateshyadav270@gmail.com>
2026-02-08 23:37:14 +0530

    Initial commit - branding_page

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navigation from "./component/Navigation.jsx";
+import Hero from "./component/Hero.jsx";
+
+const App = () => {
+  return (
+  <div>
+    <Navigation />
+    <Hero />
+  </div>
+  );
+};
+
+export default App;
